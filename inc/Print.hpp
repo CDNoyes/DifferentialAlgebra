f84146685ebac8f81f0f23a1f9e15e962aec260f
Connor Noyes <aero.noyes@gmail.com>
2016-06-02 19:58:43 -0700

    All printState functions now depend on the same width integer, set directly in this header.

--- inc/Print.hpp
+++ inc/Print.hpp
@@ -5,9 +5,10 @@
 #include <DA\dace.h>
 using namespace DACE;
 
-void printState(const std::vector<std::string>& state){
+int width = 15;
 
-    int width = 15;
+void printState(const std::vector<std::string>& state){
+    
     std::cout << std::setw(width) << std::left << "t, s";
     for (auto& s : state)
         std::cout << std::setw(width) << std::left << s;
@@ -17,7 +18,7 @@ void printState(const std::vector<std::string>& state){
 
 void printState(const double& time, const std::vector<double>& state, const int& nDecimals = 6){
 
-    int width = 15;
+    // int width = 15;
     std::cout << std::setprecision(nDecimals) << std::setw(width) << std::left << time;
     for (auto& s : state)
         std::cout << std::setw(width) << std::left << s;
@@ -27,7 +28,7 @@ void printState(const double& time, const std::vector<double>& state, const int&
 
 void printState(const double& time, const std::vector<DA>& state, const int& nDecimals = 6){
 
-    int width = 15;
+    // int width = 15;
     std::cout << std::setprecision(nDecimals) << std::setw(width) << std::left << time;
     for (auto& s : state)
         std::cout << std::setw(width) << std::left << s.cons();
@@ -38,13 +39,10 @@ void printState(const double& time, const std::vector<DA>& state, const int& nDe
 
 void printState(const double& time, const std::vector<DA>& state, const std::vector<double> evalPoint, const int& nDecimals = 6){
 
-    int width = 15;
+    // int width = 15;
     std::cout << std::setprecision(nDecimals) << std::setw(width) << std::left << time;
     for (auto& s : state)
         std::cout << std::setw(width) << std::left << s.eval(evalPoint);
     std::cout << "\n";
 
 };
-
-/* It might be overkill but a Printer class could keep track of the number of states, and notice if they change.
-    It could also record the number of prints and print the header info every n prints*/
