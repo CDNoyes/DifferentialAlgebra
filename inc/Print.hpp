561c4b7190a149f5c14b3d29b5bcc684fa342540
Connor Noyes <aero.noyes@gmail.com>
2016-06-26 18:46:11 -0700

    Added additional functions to Print, including new functions to write to a file instead of cout.

--- inc/Print.hpp
+++ inc/Print.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include<string>
+#include<iomanip>
+#include<iostream>
+#include<fstream>
 
 #include <DA\dace.h>
 using namespace DACE;
@@ -8,8 +11,17 @@ using namespace DACE;
 int width = 15;
 
 void printState(const std::vector<std::string>& state){
-    
+    // Prints header including time, and states in state vector
     std::cout << std::setw(width) << std::left << "t, s";
+    for (auto& s : state)
+        std::cout << std::setw(width) << std::left << s;
+    std::cout << "\n";
+
+}
+
+void printState(const std::string& iv, const std::vector<std::string>& state){
+    // Prints header with independent variable text in iv and state text in state
+    std::cout << std::setw(width) << std::left << iv;
     for (auto& s : state)
         std::cout << std::setw(width) << std::left << s;
     std::cout << "\n";
@@ -18,7 +30,6 @@ void printState(const std::vector<std::string>& state){
 
 void printState(const double& time, const std::vector<double>& state, const int& nDecimals = 6){
 
-    // int width = 15;
     std::cout << std::setprecision(nDecimals) << std::setw(width) << std::left << time;
     for (auto& s : state)
         std::cout << std::setw(width) << std::left << s;
@@ -28,7 +39,6 @@ void printState(const double& time, const std::vector<double>& state, const int&
 
 void printState(const double& time, const std::vector<DA>& state, const int& nDecimals = 6){
 
-    // int width = 15;
     std::cout << std::setprecision(nDecimals) << std::setw(width) << std::left << time;
     for (auto& s : state)
         std::cout << std::setw(width) << std::left << s.cons();
@@ -39,10 +49,36 @@ void printState(const double& time, const std::vector<DA>& state, const int& nDe
 
 void printState(const double& time, const std::vector<DA>& state, const std::vector<double> evalPoint, const int& nDecimals = 6){
 
-    // int width = 15;
     std::cout << std::setprecision(nDecimals) << std::setw(width) << std::left << time;
     for (auto& s : state)
         std::cout << std::setw(width) << std::left << s.eval(evalPoint);
     std::cout << "\n";
 
 };
+
+
+
+void writeState(const double& time, const std::vector<double>& state, const int& nDecimals = 6){
+    
+    std::ofstream outputFile;
+    
+    outputFile.open("res/temp.txt", std::fstream::out | std::fstream::app);
+    
+    outputFile << std::setprecision(nDecimals) << std::setw(width) << std::left << time;
+    for (auto& s : state)
+        outputFile << std::setw(width) << std::left << s;
+    outputFile << "\n";
+    outputFile.close();
+}
+
+// void writeState(const double& time, const std::vector<double>& state, const std::vector<std::string>& filename, const int& nDecimals = 6){
+    
+    // std::ofstream outputFile;
+    
+    // outputFile.open("res/" + filename);
+    
+    // outputFile << std::setprecision(nDecimals) << std::setw(width) << std::left << time;
+    // for (auto& s : state)
+        // outputFile << std::setw(width) << std::left << s;
+    // outputFile << "\n";
+// }
